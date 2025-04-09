53f351734e45992bf8fbf26354d99168391cb124
Saurabh Burde <87974941+sdburde@users.noreply.github.com>
2025-04-09 11:16:17 +0530

    Delete otter-detection directory

--- otter-detection/server/static/devel/src/mwc/textfield.jsx
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-import "@material/mwc-textfield"
-
-import React, { useRef, useState, useEffect } from "react"
-
-export function TextField({ ...props }) {
-    return <mwc-textfield {...props}></mwc-textfield>
-}
